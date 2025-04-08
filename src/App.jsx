b7c5dad78de5f4a003231ddab9296e4a3b8df499
reinarkajava <reinar.kajava@voco.ee>
2025-04-08 12:37:27 +0300

    Ylesanne 1 Kujunduse rakendamine komponendis

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+const App = () => {
+  return (
+    <div className="App">
+      < ExpenseItem />
+      < ExpenseItem />
+      < ExpenseItem />
+    </div>
+
+    
+  );
+}
+
+
+
+
+export default App;
